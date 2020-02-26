7c6a1bc59d900fd0b6224e110bd654da5760005e
sushma <sushmapreethi95@gmail.com>
2020-02-26 12:18:53 -0600

    Important files

---
+++ EventPlanner/EventPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventPlanner
+//
+//  Created by Student on 2/26/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
